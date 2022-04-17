7bb8fc2d419e7ba3f92afd850346e71b3f0d4037
97baek <170410_@naver.com>
2022-04-17 16:38:24 +0900

    Config: eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // uses typescript-specific linting rules
+    'plugin:react/recommended', // uses react-specific linting rules
+    'prettier/react', // disables react-specific linting rules that conflict with prettier
+    'plugin:prettier/recommended', // uses react-specific linting rules],
+  ],
+  root: true,
+  plugins: ['@typescript-eslint', 'import'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
@@ -13,4 +22,14 @@ module.exports = {
       },
     },
   ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
